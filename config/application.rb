08a152ee43adec32dec6092808c05cb6e6708686
Manos Fountoulakis <fountoulakismanos@gmail.com>
2017-12-16 15:35:45 -0500

    installs and configures RackCors for emer data

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,15 @@ module MiniBookingsync
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.middleware.use Rack::Cors do
+      allow do
+        origins "*"
+        resource "*",
+          :headers => :any,
+          :methods => [:get, :post, :put, :delete, :options, :patch]
+        end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
